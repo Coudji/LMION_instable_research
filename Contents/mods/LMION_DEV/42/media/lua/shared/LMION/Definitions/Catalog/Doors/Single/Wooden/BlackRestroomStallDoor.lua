b169fe336d5d703ca0f38f96447188eb673949c0
Coudji <106971118+Coudji@users.noreply.github.com>
2026-09-04 19:41:32 +0200

    Add Black Restroom Stall Door definition

---
+++ Contents/mods/LMION_DEV/42/media/lua/shared/LMION/Definitions/Catalog/Doors/Single/Wooden/BlackRestroomStallDoor.lua
@@ -0,0 +1,16 @@
+return {
+    definitionId = "Doors.Wood.BlackRestroomStallDoor",
+    entity = "Base.BlackRestroomStallDoor",
+    inherits = "Doors.Wood.Restroom",
+
+    geometry = {
+        N = {
+            closed = "fixtures_doors_02_25",
+            open = "fixtures_doors_02_27",
+        },
+        W = {
+            closed = "fixtures_doors_02_24",
+            open = "fixtures_doors_02_26",
+        },
+    },
+}
