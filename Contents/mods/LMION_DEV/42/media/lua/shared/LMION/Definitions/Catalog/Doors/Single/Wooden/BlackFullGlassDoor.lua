3c2c85dddcaf2980313afbcc103ad619d83330e8
Coudji <106971118+Coudji@users.noreply.github.com>
2026-09-04 19:41:26 +0200

    Add Black Full Glass Door definition

---
+++ Contents/mods/LMION_DEV/42/media/lua/shared/LMION/Definitions/Catalog/Doors/Single/Wooden/BlackFullGlassDoor.lua
@@ -0,0 +1,16 @@
+return {
+    definitionId = "Doors.Wood.BlackFullGlassDoor",
+    entity = "Base.BlackFullGlassDoor",
+    inherits = "Doors.Wood.FullGlass",
+
+    geometry = {
+        N = {
+            closed = "fixtures_doors_01_41",
+            open = "fixtures_doors_01_43",
+        },
+        W = {
+            closed = "fixtures_doors_01_40",
+            open = "fixtures_doors_01_42",
+        },
+    },
+}
