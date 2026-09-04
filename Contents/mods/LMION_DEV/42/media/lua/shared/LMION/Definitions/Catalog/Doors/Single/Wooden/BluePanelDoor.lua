4c6acbba788c5ed3e0879fe20c86099ffc834a41
Coudji <106971118+Coudji@users.noreply.github.com>
2026-09-04 19:41:44 +0200

    Add Blue Panel Door definition

---
+++ Contents/mods/LMION_DEV/42/media/lua/shared/LMION/Definitions/Catalog/Doors/Single/Wooden/BluePanelDoor.lua
@@ -0,0 +1,16 @@
+return {
+    definitionId = "Doors.Wood.BluePanelDoor",
+    entity = "Base.BluePanelDoor",
+    inherits = "Doors.Wood.FourPanels",
+
+    geometry = {
+        N = {
+            closed = "fixtures_doors_02_1",
+            open = "fixtures_doors_02_3",
+        },
+        W = {
+            closed = "fixtures_doors_02_0",
+            open = "fixtures_doors_02_2",
+        },
+    },
+}
