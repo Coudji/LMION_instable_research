bbeeb8e25268c7fbaf25d10759b35c18e4aedb1c
Coudji <106971118+Coudji@users.noreply.github.com>
2026-09-04 19:41:55 +0200

    Register second simple-door catalog batch

--- Contents/mods/LMION_DEV/42/media/lua/shared/LMION/Definitions/BuiltinContent.lua
+++ Contents/mods/LMION_DEV/42/media/lua/shared/LMION/Definitions/BuiltinContent.lua
@@ -4,7 +4,11 @@ return {
         require "LMION/Definitions/Defaults/Doors/MetalOneGlass",
         require "LMION/Definitions/Defaults/Doors/MetalService",
         require "LMION/Definitions/Defaults/Doors/MetalTwoGlass",
+        require "LMION/Definitions/Defaults/Doors/WoodBase",
         require "LMION/Definitions/Defaults/Doors/WoodFourPanels",
+        require "LMION/Definitions/Defaults/Doors/WoodFullGlass",
+        require "LMION/Definitions/Defaults/Doors/WoodOneGlass",
+        require "LMION/Definitions/Defaults/Doors/WoodRestroom",
     },
 
     definitions = {
@@ -12,6 +16,10 @@ return {
         require "LMION/Definitions/Catalog/Doors/Single/Metal/BlackServiceDoor",
         require "LMION/Definitions/Catalog/Doors/Single/Metal/BlackTwoPaneMetalDoor",
         require "LMION/Definitions/Catalog/Doors/Single/Metal/BlueServiceDoor",
+        require "LMION/Definitions/Catalog/Doors/Single/Wooden/BlackFullGlassDoor",
+        require "LMION/Definitions/Catalog/Doors/Single/Wooden/BlackRestroomStallDoor",
+        require "LMION/Definitions/Catalog/Doors/Single/Wooden/BlueDoorWithWindow",
+        require "LMION/Definitions/Catalog/Doors/Single/Wooden/BluePanelDoor",
         require "LMION/Definitions/Catalog/Doors/Single/Wooden/WhitePanelDoor",
     },
 }
