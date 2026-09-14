9bc4e4f635cf86c54f338941f7f2c9edcf8ece49
Coudji <106971118+Coudji@users.noreply.github.com>
2026-09-14 04:56:56 +0200

    Use split Garage finalizer in Build hook

--- Contents/mods/LMION_DEV/42/media/lua/server/LMION/Hooks/Build/DoorSetInfo.lua
+++ Contents/mods/LMION_DEV/42/media/lua/server/LMION/Hooks/Build/DoorSetInfo.lua
@@ -1,28 +1,58 @@
 require "BuildingObjects/ISBuildIsoEntity"
-local SingleTileDoorBuildProfile=require "LMION/Services/Build/SingleTileDoorBuildProfile"
-local SingleTileDoorFinalizer=require "LMION/Services/Build/SingleTileDoorFinalizer"
-local LargeGateBuildProfile=require "LMION/Services/Build/LargeGateBuildProfile"
-local LargeGateFinalizer=require "LMION/Services/Build/LargeGateFinalizer"
-local GarageBuildFinalizer=require "LMION/Services/Build/GarageBuildFinalizer"
 
-local function gameScript(buildObject)
- local spriteScript=buildObject and buildObject.objectInfo and buildObject.objectInfo:getScript() or nil
- return spriteScript and spriteScript:getParent() or nil
+local GarageBuildFinalizer = require "LMION/Services/Build/Garage/GarageBuildFinalizer"
+local LargeGateBuildProfile = require "LMION/Services/Build/LargeGateBuildProfile"
+local LargeGateFinalizer = require "LMION/Services/Build/LargeGateFinalizer"
+local SingleTileDoorBuildProfile = require "LMION/Services/Build/SingleTileDoorBuildProfile"
+local SingleTileDoorFinalizer = require "LMION/Services/Build/SingleTileDoorFinalizer"
+
+local function getGameScript(buildObject)
+    local spriteScript = buildObject
+        and buildObject.objectInfo
+        and buildObject.objectInfo:getScript()
+        or nil
+
+    return spriteScript and spriteScript:getParent() or nil
 end
+
 if not ISBuildIsoEntity._lmionV3DoorSetInfoInstalled then
- ISBuildIsoEntity._lmionV3DoorSetInfoInstalled=true
- local previous=ISBuildIsoEntity.setInfo
- ISBuildIsoEntity.setInfo=function(self,square,north,sprite,openSprite)
-  local script=gameScript(self)
-  local single=SingleTileDoorBuildProfile.getByGameScript(script)
-  local large=LargeGateBuildProfile.getByGameScript(script)
-  local garage=GarageBuildFinalizer.getProfile(self)
-  if garage and not GarageBuildFinalizer.beforeSetInfo(self,garage) then error("LMION Garage extra-resource consumption failed") end
-  local result=previous(self,square,north,sprite,openSprite)
-  if single then SingleTileDoorFinalizer.finalize(square,single,self.craftRecipe,self.character)
-  elseif large then LargeGateFinalizer.finalize(square,large,self.craftRecipe,self.character)
-  elseif garage then GarageBuildFinalizer.finalize(self,square,garage) end
-  return result
- end
- print("[LMION:DEV] shared door Build setInfo hook installed")
+    ISBuildIsoEntity._lmionV3DoorSetInfoInstalled = true
+
+    local previousSetInfo = ISBuildIsoEntity.setInfo
+
+    ISBuildIsoEntity.setInfo = function(self, square, north, sprite, openSprite)
+        local gameScript = getGameScript(self)
+        local singleProfile = SingleTileDoorBuildProfile.getByGameScript(gameScript)
+        local largeGateProfile = LargeGateBuildProfile.getByGameScript(gameScript)
+        local garageProfile = GarageBuildFinalizer.getProfile(self)
+
+        if garageProfile ~= nil
+            and not GarageBuildFinalizer.beforeSetInfo(self, garageProfile) then
+            error("LMION Garage extra-resource consumption failed")
+        end
+
+        local result = previousSetInfo(self, square, north, sprite, openSprite)
+
+        if singleProfile ~= nil then
+            SingleTileDoorFinalizer.finalize(
+                square,
+                singleProfile,
+                self.craftRecipe,
+                self.character
+            )
+        elseif largeGateProfile ~= nil then
+            LargeGateFinalizer.finalize(
+                square,
+                largeGateProfile,
+                self.craftRecipe,
+                self.character
+            )
+        elseif garageProfile ~= nil then
+            GarageBuildFinalizer.finalize(self, square, garageProfile)
+        end
+
+        return result
+    end
+
+    print("[LMION:DEV] shared door Build setInfo hook installed")
 end
