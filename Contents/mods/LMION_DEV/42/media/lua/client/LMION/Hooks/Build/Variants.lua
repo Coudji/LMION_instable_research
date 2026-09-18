dcc45e81f52080e9e942ecc709ee37b1c911f8d2
Coudji <106971118+Coudji@users.noreply.github.com>
2026-09-18 13:11:52 +0200

    Delegate build recipe options UI composition

--- Contents/mods/LMION_DEV/42/media/lua/client/LMION/Hooks/Build/Variants.lua
+++ Contents/mods/LMION_DEV/42/media/lua/client/LMION/Hooks/Build/Variants.lua
@@ -1,36 +1,7 @@
-require "Entity/ISUI/BuildRecipe/ISBuildRecipePanel"
 require "Entity/ISUI/BuildRecipe/ISBuildPanel"
-require "LMION/UI/Build/VariantSelector"
+require "LMION/Hooks/Build/RecipeOptions"
 
 local VariantState = require "LMION/Services/Build/VariantState"
-
-if not ISBuildRecipePanel._lmionV3VariantSelectorInstalled then
-    ISBuildRecipePanel._lmionV3VariantSelectorInstalled = true
-
-    local previousCreateDynamicChildren = ISBuildRecipePanel.createDynamicChildren
-
-    ISBuildRecipePanel.createDynamicChildren = function(self)
-        previousCreateDynamicChildren(self)
-
-        local group = VariantState.getGroupFromLogic(self.logic)
-        if group == nil or #group.members < 2 or self.rootTable == nil then
-            self.lmionBuildVariantSelector = nil
-            return
-        end
-
-        local selector = LMIONBuildVariantSelector:new(
-            self.player,
-            self.logic,
-            self
-        )
-        selector:initialise()
-        selector:instantiate()
-
-        self.lmionBuildVariantSelector = selector
-        self.rootTable:setElement(0, 1, selector)
-        self:xuiRecalculateLayout()
-    end
-end
 
 if not ISBuildPanel._lmionV3VariantRepeatInstalled then
     ISBuildPanel._lmionV3VariantRepeatInstalled = true
