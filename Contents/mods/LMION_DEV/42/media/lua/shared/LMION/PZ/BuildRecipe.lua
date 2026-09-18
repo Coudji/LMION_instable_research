1dd155ddf7dc961acd3c1de9fd23816275a19252
Coudji <106971118+Coudji@users.noreply.github.com>
2026-09-18 02:33:58 +0200

    Add narrow PZ build recipe adapter

---
+++ Contents/mods/LMION_DEV/42/media/lua/shared/LMION/PZ/BuildRecipe.lua
@@ -0,0 +1,35 @@
+local BuildRecipe = {}
+
+local function getEntityShortName(entityId)
+    if type(entityId) ~= "string" or entityId == "" then
+        return nil
+    end
+
+    return string.match(entityId, "^[^.]+%.(.+)$") or entityId
+end
+
+function BuildRecipe.getByName(recipeName)
+    if type(recipeName) ~= "string"
+        or recipeName == ""
+        or ScriptManager == nil
+        or ScriptManager.instance == nil then
+        return nil
+    end
+
+    return ScriptManager.instance:getBuildableRecipe(recipeName)
+end
+
+function BuildRecipe.getByEntityId(entityId)
+    local recipeName = getEntityShortName(entityId)
+    if recipeName == nil then
+        return nil
+    end
+
+    return BuildRecipe.getByName(recipeName)
+end
+
+function BuildRecipe.getNameForEntityId(entityId)
+    return getEntityShortName(entityId)
+end
+
+return BuildRecipe
